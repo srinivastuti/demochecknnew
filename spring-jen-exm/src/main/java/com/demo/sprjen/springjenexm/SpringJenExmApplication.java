85d160839f4ae31b6537ce35736021ee02db461e
god <god@god-PC>
2019-08-29 20:39:21 +0530

    first commit

---
+++ spring-jen-exm/src/main/java/com/demo/sprjen/springjenexm/SpringJenExmApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sprjen.springjenexm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenExmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenExmApplication.class, args);
+	}
+
+}
